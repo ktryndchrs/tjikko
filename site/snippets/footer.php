f42364ac151ca0b606d7bb5127053a9b6a72d608
Catherine <catherine.desrochers@gmail.com>
2021-04-22 20:20:20 -0600

    Social media links in the Footer

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,14 +1,27 @@
-<footer class="app-container pt-16 text-sm leading-6 text-gray-500 flex items-stretch sm:justify-between sm:space-x-2 flex-col sm:flex-row sm:items-end ">
+<footer class="app-container pt-16 text-sm leading-6 text-gray-500 flex items-stretch sm:justify-between sm:space-x-2 flex-col sm:flex-row">
     <div class="text-left">
       <div> <?= safeContact( $pages->find('a-propos')->Email() ) ?> </div>
       <div class="mt-2 sm:mt-0">
         <?= safeContact( $pages->find('a-propos')->Phone(), "tel" ) ?>
+        <?php
+          $links =  $pages->find('a-propos')->social()->toStructure();
+          if ($links->isNotEmpty()):
+        ?>
+          <ul class="mt-2 sm:mt-0 flex space-x-1 items-center text-gray-600">
+            <?php foreach ( $links as $link ): ?>
+              <li>
+                <a href="<?= $link->url() ?>" target="_blank" aria-label="Tjikko on <?php $link->platform() ?>" class="block box-content h-5 w-5 p-1.5 md:p-1">
+                  <?= svg('assets/images/'.$link->platform().'.svg') ?>
+                </a>
+              </li>
+            <?php endforeach; ?>
+          </ul>
+        <?php endif; ?>
       </div>
-      <div class="mt-6 sm:mt-0"> <?= $pages->find('a-propos')->footerL()->kb()->kt()->inline() ?> </div>
     </div>
     <div class="text:left sm:text-right mt-6 sm:mt-0">
-      <?= $pages->find('a-propos')->footerR()->kb()->kt()->inline() ?>
-      <div id="wcb" class="carbonbadge"></div>
+      <div class="mt-6 sm:mt-0"> <?= $pages->find('a-propos')->footerR()->kb()->kt()->inline() ?> </div>
+      <div id="wcb" class="carbonbadge mt-2 sm:mt-0"></div>
     </div>
   </footer>
   
